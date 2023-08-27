006fc0f2bdcfa60dc3261b3631fe78975e2c4846
Dynamicpedro <taiwoademola881@gmail.com>
2023-08-27 15:35:38 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _strcat - concatenates two strings
@@ -24,6 +24,6 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 
-	dest[i] = '\0'
+	dest[i] = '\0';
 	return (dest);
 }
